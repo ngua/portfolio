06650b5fe265586bcea6728f7ccb476b57547a56
ngua <rory.hayford@gmail.com>
2020-04-17 17:03:56 +0700

    Moved static files to named volume to avoid file ownership issues

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py collectstatic --no-input --settings=$DJANGO_SETTINGS_MODULE
+python manage.py collectstatic --no-input --clear --settings=$DJANGO_SETTINGS_MODULE
 python manage.py migrate --no-input --settings=$DJANGO_SETTINGS_MODULE
 
-exec "@"
+exec "$@"
